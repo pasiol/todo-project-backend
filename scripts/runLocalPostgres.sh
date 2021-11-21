ccdaa4e1bd1e837713f96bc46f2cb3fbffac80c3
Pasi Ollikainen <pasi.ollikainen@pm.me>
2021-11-21 08:55:05 +0200

    2.08

--- scripts/runLocalPostgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd manifests/postgres/local
-docker stop test-postgres
-docker build -t test-postgres .
-docker run --rm --name test-postgres -d -p 5432:5432 test-postgres
-cd ../..
-docker ps | grep test-postgres
